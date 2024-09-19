fb0f3a98ff09d704e91e7c5d5256e8598483c870
DrowAnn <rs19990801@gmail.com>
2024-09-19 15:46:11 -0500

    creacion del header

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,20 +1,27 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  makeStateKey,
-} from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-
-const HEADER_STATE_KEY = makeStateKey<string>('header');
+import { AuthService } from '../../servicios/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  providers: [AuthService],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  constructor(
+    private readonly authService: AuthService,
+    private router: Router
+  ) {}
+
+  cerrarSesion() {
+    this.authService.eliminarToken();
+    this.router.navigate(['login']);
+  }
+}
