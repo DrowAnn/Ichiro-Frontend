99edd1ea8b02d57b005a5e1c84365ef47bd957db
DrowAnn <rs19990801@gmail.com>
2024-10-04 17:16:22 -0500

    Inicio del modulo para Hijos de los colaboradores

--- src/app/datos-sociodemograficos/hijos/hijos.component.ts
+++ src/app/datos-sociodemograficos/hijos/hijos.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { RouterModule } from '@angular/router';
+import { Colaborador } from '../../colaboradores/colaboradores.dto';
+import { ColaboradoresService } from '../../servicios/colaboradores/colaboradores.service';
 
 @Component({
   selector: 'app-hijos',
   standalone: true,
-  imports: [],
+  imports: [
+    MatListModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    CommonModule,
+    RouterModule,
+  ],
+  providers: [ColaboradoresService],
   templateUrl: './hijos.component.html',
   styleUrl: './hijos.component.scss',
 })
-export default class HijosComponent {}
+export default class HijosComponent {
+  listaColaboradores = signal<Colaborador[]>([]);
+  areas: string[] = [
+    'Asistencial',
+    'Administrativo',
+    'Financiero',
+    'Estetica',
+    'Comercial',
+    'Recepcion',
+    'Programacion',
+    'Instrumental_Y_Central_De_Lavado',
+    'Farmacia',
+    'Hospitalizacion',
+    'Servicios_Generales',
+    'Mantenimiento',
+    'Medicina_General',
+    'Rayos_X',
+  ].sort();
+
+  constructor(private readonly colaboradoresService: ColaboradoresService) {
+    this.colaboradoresService.obtenerColaboradores().subscribe({
+      next: (response: Colaborador[]) => {
+        this.listaColaboradores.set(response);
+        this.listaColaboradores().sort((a, b) => {
+          if ((a?.area ?? '').localeCompare(b?.area ?? '') == 0) {
+            return a.primerNombre.localeCompare(b.primerNombre);
+          }
+          return (a?.area ?? '').localeCompare(b?.area ?? '');
+        });
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+}
