fe9107dccf3f3bd6f0cdc5b216b70bd0cc6f30e2
DrowAnn <rs19990801@gmail.com>
2024-10-22 18:07:44 -0500

    creacion de logica para el calculo de las horas trabajadas

--- src/app/jornadas-laborales/horarios/formulario-horarios/formulario-horarios.component.ts
+++ src/app/jornadas-laborales/horarios/formulario-horarios/formulario-horarios.component.ts
@@ -1,18 +1,19 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  signal,
+} from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import { JornadasLaboralesService } from '../../../servicios/jornadas-laborales/jornadas-laborales.service';
 import { provideMomentDatetimeAdapter } from '@ng-matero/extensions-moment-adapter';
 import { MatRadioModule } from '@angular/material/radio';
-import {
-  MtxCalendarView,
-  MtxDatetimepickerMode,
-  MtxDatetimepickerModule,
-  MtxDatetimepickerType,
-} from '@ng-matero/extensions/datetimepicker';
+import { MtxDatetimepickerModule } from '@ng-matero/extensions/datetimepicker';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   FormsModule,
   ReactiveFormsModule,
@@ -23,10 +24,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {
-  MatCheckboxChange,
-  MatCheckboxModule,
-} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSliderModule } from '@angular/material/slider';
 // import { CustomHeader } from './custom-header.component';
 
@@ -103,7 +101,7 @@ export default class FormularioHorariosComponent {
       tiempoCena: [null],
       horaOtroReceso: [null],
       tiempoReceso: [null],
-      horasProgramadas: [null],
+      horasProgramadas: new FormControl({ value: null, disabled: true }),
       areaFuncionProgramada: [null],
       observacionNovedadesHorarios: [null],
       nombreModificadorHorarios: [null],
@@ -111,9 +109,54 @@ export default class FormularioHorariosComponent {
   }
 
   fechaIngesoSignal = signal<string>('');
+  fechaSalidaSignal = signal<string>('');
+  timepoDesayunoSignal = signal<number>(0);
+  timepoAlmuerzoSignal = signal<number>(0);
+  timepoCenaSignal = signal<number>(0);
+  timepoRecesoSignal = signal<number>(0);
 
   fechaIngreso(fechaIngresada: string) {
     this.fechaIngesoSignal.set(fechaIngresada.toString());
-    console.log(this.fechaIngesoSignal());
   }
+
+  fechaSalida(fechaSalida: string) {
+    this.fechaSalidaSignal.set(fechaSalida.toString());
+  }
+
+  tiempoDesayuno(evento: Event) {
+    const inputElement = evento.target as HTMLInputElement;
+    this.timepoDesayunoSignal.set(inputElement.valueAsNumber);
+  }
+
+  tiempoAlmuerzo(evento: Event) {
+    const inputElement = evento.target as HTMLInputElement;
+    this.timepoAlmuerzoSignal.set(inputElement.valueAsNumber);
+  }
+
+  tiempoCena(evento: Event) {
+    const inputElement = evento.target as HTMLInputElement;
+    this.timepoCenaSignal.set(inputElement.valueAsNumber);
+  }
+
+  tiempoReceso(evento: Event) {
+    this.timepoRecesoSignal.set(
+      (evento.target as HTMLInputElement).valueAsNumber
+    );
+    console.log(this.timepoRecesoSignal());
+  }
+
+  horasProgramadasSignal = computed(() => {
+    const tiempoMilisegundos: number =
+      new Date(this.fechaSalidaSignal()).getTime() -
+      new Date(this.fechaIngesoSignal()).getTime();
+    const tiempoCalculos = isNaN(tiempoMilisegundos) ? 0 : tiempoMilisegundos;
+    const timepoHoras: number = tiempoCalculos / 3600000;
+    const tiempoTotal: number =
+      timepoHoras -
+      (this.timepoDesayunoSignal() ?? 0) / 60 -
+      (this.timepoAlmuerzoSignal() ?? 0) / 60 -
+      (this.timepoCenaSignal() ?? 0) / 60 -
+      (this.timepoRecesoSignal() ?? 0) / 60;
+    return tiempoTotal;
+  });
 }
